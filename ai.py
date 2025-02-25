64dd941a53a9c97f2997a959786726069a21d1ba
c4ffein <c4ffein@gmail.com>
2025-02-25 22:43:13 +0100

    added 3-7

--- ai.py
+++ ai.py
@@ -37,6 +37,8 @@ COLOR_LEN = 4
 
 
 CLAUDE_MODELS = [
+    ["claude-3-7-sonnet-latest"],
+    ["claude-3-7-haiku-latest"],
     ["claude-3-5-sonnet-20241022"],
     ["claude-3-5-haiku-20241022"],
     ["claude-3-opus-20240229"],
