54889cde4551d7e0ee762d17b02339dcf6fe1998
c4ffein <c4ffein@gmail.com>
2025-11-25 20:04:15 +0100

    updated models: Opus/Haiku 4.5

--- ai.py
+++ ai.py
@@ -47,9 +47,11 @@ Color = Enum("Color", [(k, f"\033[{v}m") for k, v in colors.items()])
 ClaudeModel = namedtuple("ClaudeModel", ("local_name", "remote_handle"))
 # fmt: off
 CLAUDE_MODELS_BASE = [
+    [ "claude-opus-4-5",   "claude-opus-4-5-20251101"   ],
+    [ "claude-haiku-4-5",  "claude-haiku-4-5-20251001"  ],
+    [ "claude-sonnet-4-5", "claude-sonnet-4-5-20250929" ],
     [ "claude-opus-4-1",   "claude-opus-4-1-20250805"   ],
     [ "claude-opus-4",     "claude-opus-4-20250514"     ],
-    [ "claude-sonnet-4-5", "claude-sonnet-4-5-20250929" ],
     [ "claude-sonnet-4",   "claude-sonnet-4-20250514"   ],
     [ "claude-sonnet-3-7", "claude-3-7-sonnet-latest"   ],
     [ "claude-haiku-3-7",  "claude-3-7-haiku-latest"    ],
